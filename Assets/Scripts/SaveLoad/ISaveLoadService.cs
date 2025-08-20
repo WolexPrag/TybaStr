474290229dc26d12724ddc0145764cc1989b1789
Wolex <wolexprag@gmail.com>
2025-08-20 19:43:37 +0300

    Implement SaveLoadService and JsonToFileSaveLoadService

---
+++ Assets/Scripts/SaveLoad/ISaveLoadService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TybaStr.SaveLoad
+{
+    public interface ISaveLoadService
+    {
+        void Save(string key,object data, Action<bool> callback = null);
+        void Load<T>(string key,Action<T> callback);
+    }
+}
---
+++ Assets/Scripts/SaveLoad/JsonToFileSaveLoadService.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace TybaStr.SaveLoad
+{
+    public class JsonToFileSaveLoadService : ISaveLoadService
+    {
+
+        public void Save(string key, object data, Action<bool> callback = null)
+        {
+            string path = BuildPath(key);
+            string json = JsonConvert.SerializeObject(data);
+
+            using(var fileStream = new StreamWriter(path))
+            {
+                fileStream.Write(json);
+            }
+
+            callback?.Invoke(true);
+        }
+        public void Load<T>(string key, Action<T> callback)
+        {
+            string path = BuildPath(key);
+
+            using (var fileStream = new StreamReader(path))
+            {
+                string json = fileStream.ReadToEnd();
+                T data = JsonConvert.DeserializeObject<T>(json);
+
+                callback?.Invoke(data);
+            }
+        }
+        private string BuildPath(string key)
+        {
+            return Path.Combine(Application.persistentDataPath, key);
+        }
+    }
+}
