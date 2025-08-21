15e25290511df0833e4cfaa9fadcee9b66d11855
Wolex <wolexprag@gmail.com>
2025-08-21 22:06:07 +0300

    fixup! Maked IView

--- Assets/Scripts/UserProfile.cs
+++ Assets/Scripts/UserProfile.cs
@@ -10,11 +10,11 @@ namespace TybaStr
     [Serializable]
     public class UserProfile
     {
-        [SerializeField] private ReactiveProperty<string> _name;
+        [SerializeField] private readonly ReactiveProperty<string> _name = new();
         public Observable<string> OnChangeName => _name;
         public string Name { get { return _name.Value; } set { _name.Value = value; } }
 
-        [SerializeField] private ReactiveProperty<Image> _Icon;
+        [SerializeField] private readonly ReactiveProperty<Image> _Icon = new();
         public Observable<Image> OnChangeIcon => _Icon;
         public Image Icon { get { return _Icon.Value; } set { _Icon.Value = value; } }
     }
