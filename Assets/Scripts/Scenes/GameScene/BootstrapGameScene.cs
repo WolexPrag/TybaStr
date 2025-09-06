3dec745ea75af5fe2f2ecab95692579d583980bb
Wolex <wolexprag@gmail.com>
2025-09-06 13:56:26 +0300

    Refactor BootstrapGameScene: initialize ViewModel with SaveLoadService , InputActions, and player camera

--- Assets/Scripts/Scenes/GameScene/BootstrapGameScene.cs
+++ Assets/Scripts/Scenes/GameScene/BootstrapGameScene.cs
@@ -1,19 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using R3;
 using TybaStr.MVVM.GameScene;
+using TybaStr.SaveLoad;
+using TybaStr.InputActions;
 
 
-public class BootstrapPlayScene<T> :MonoBehaviour
+public class BootstrapGameScene :MonoBehaviour
 {
+    [SerializeField] private GameInputActions _inputActions;
+    [SerializeField] private PlayerCamera _playerCamera;
+    
+
     [SerializeField] private ViewGameScene _view;
-    [SerializeField] private ViewModelGameScene _viewModel;
-    [SerializeField] private ModelGameScene _model;
+    private ViewModelGameScene _viewModel;
+    private ModelGameScene _model;
+
+
     private void Awake()
     {
+        Init();
+        Activate();
+    }
+    private void Init()
+    {
         _model = new ModelGameScene();
+        _viewModel = new ViewModelGameScene(new JsonToFileSaveLoadService());
+        _inputActions = new GameInputActions();
         _viewModel.Init(_model);
         _view.Init(_viewModel);
+    }
+    private void Activate()
+    {
+        _inputActions.Enable();
+        _playerCamera.SetInput(_inputActions.Camera.Hold,_inputActions.Camera.Point);
     }
 }
