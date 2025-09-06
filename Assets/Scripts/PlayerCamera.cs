9c4396a55144962641a8f4933b48af3c910da50f
Wolex <wolexprag@gmail.com>
2025-09-06 13:39:23 +0300

    Created PlayerCamera and realized drag her

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using UnityEngine.InputSystem;
+using UnityEngine;
+using R3;
+
+
+[RequireComponent(typeof(Camera))]
+public class PlayerCamera : MonoBehaviour
+{
+    public Camera Camera => _camera ??= Camera.main;
+    private Camera _camera;
+
+    private InputAction _press;
+    private InputAction _point;
+    private CompositeDisposable _disposables = new();
+
+    private bool isDrag;
+    private Vector2 _startPressPoint;
+    private Vector2 PointToWorld => Camera.ScreenToWorldPoint(_point.ReadValue<Vector2>());
+    
+
+    public void SetInput(InputAction press,InputAction point)
+    {
+        _press = press;
+        _point = point;
+        Observable.FromEvent<InputAction.CallbackContext>(
+            h => _press.started += h,
+            h => press.started -= h).Subscribe(OnPressStarted).AddTo(_disposables);
+
+        Observable.FromEvent<InputAction.CallbackContext>(
+            h => _press.canceled += h,
+            h => press.canceled -= h).Subscribe(OnPressCanceled).AddTo(_disposables);
+    }
+
+    private void Update()
+    {
+        if (isDrag) {
+            OnDrag();
+        }
+    }
+    private void OnPressStarted(InputAction.CallbackContext context)
+    {
+        _startPressPoint = PointToWorld;
+        isDrag = true;
+    }
+    private void OnPressCanceled(InputAction.CallbackContext context)
+    {
+        isDrag = false;
+    }
+    private void OnDrag()
+    {
+        Move(_startPressPoint - PointToWorld);
+    }
+    private void ResetInput()
+    {
+        if (!_disposables.IsDisposed)
+        {
+            _disposables.Dispose();
+        }
+    }
+    private void Move(Vector2 input)
+    {
+        transform.position += new Vector3(input.x,input.y,0);
+    }
+
+    private void OnDisable()
+    {
+        ResetInput();
+    }
+}
+
+
