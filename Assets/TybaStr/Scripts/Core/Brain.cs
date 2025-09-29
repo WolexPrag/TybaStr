d65a6fbeb974fea3f0d3939c418ccbeff2e2a371
Wolex <wolexprag@gmail.com>
2025-09-29 21:00:04 +0300

    Maked Brain (without logic)

---
+++ Assets/TybaStr/Scripts/Core/Brain.cs
@@ -0,0 +1,17 @@
+﻿namespace TybaStr.Core
+{
+    public class Brain
+    {
+        protected Fraction _fraction;
+        protected FractionTask? _task;
+        protected Unit _unit;
+        public Brain(Fraction fraction)
+        {
+            _fraction = fraction;
+        }
+        public void Bind(Unit unit)
+        {
+            _unit = unit;
+        }
+    }
+}
