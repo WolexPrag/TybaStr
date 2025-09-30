c3cb5993e14ebe48128b49ad32a317d499bf025e
Wolex <wolexprag@gmail.com>
2025-09-30 11:33:54 +0300

    Initial FractionTask: includes TaskType and Unit

---
+++ Assets/TybaStr/Scripts/Core/FractionTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace TybaStr.Core
+{
+    public enum TypeTask
+    {
+        Use,
+        Move,
+        Attack,
+        Follow,
+        Defence,
+    }
+    [Serializable]
+    public struct FractionTask
+    {
+        [SerializeField] private Unit _unit;
+        public Unit Unit { get { return _unit; } }
+        [SerializeField] private TypeTask _type;
+        public TypeTask Type { get { return _type; } }
+
+        public FractionTask(Unit unit,TypeTask type)
+        {
+            _unit = unit;
+            _type = type;
+        }
+    }
+}
