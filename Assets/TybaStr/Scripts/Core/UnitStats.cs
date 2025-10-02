3e62c2a528de285c6f5842265b2f204bec1df922
Wolex <wolexprag@gmail.com>
2025-10-02 17:03:20 +0300

    Add UnitStats and integrate into Unit (base realization)

---
+++ Assets/TybaStr/Scripts/Core/UnitStats.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace TybaStr.Core
+{
+    [Serializable]
+    public class UnitStats
+    {
+        [SerializeField] private Unit _unit;
+        public UnitStats(Unit unit)
+        {
+            _unit = unit;
+        }
+        [SerializeField] private float _health;
+        public float Health
+        {
+            get { return _health; }
+            set { _health = value;}
+        }
+        [SerializeField] private float _midHealth;
+        public float MidHealth
+        {
+            get { return _midHealth; }
+        }
+        public float HealthBonus
+        {
+            get
+            {
+                float cofficient = Health / MidHealth;
+                if (cofficient >= 1.5)
+                {
+                    return 1.5f;
+                }
+                else if(cofficient < 0.5)
+                {
+                    return 0f;
+                }
+                return cofficient;
+            }
+        }
+        [SerializeField] private float _midSpeed;
+        public float Speed
+        {
+            get
+            {
+                return _midSpeed * HealthBonus;
+            }
+        }
+    }
+}
