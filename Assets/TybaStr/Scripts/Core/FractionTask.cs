6413669353894267f2f714215bdbd263d88d1a11
Wolex <wolexprag@gmail.com>
2025-10-09 16:10:16 +0300

    Added target in FractionTask ( and remake public get unit and type on =>)

--- Assets/TybaStr/Scripts/Core/FractionTask.cs
+++ Assets/TybaStr/Scripts/Core/FractionTask.cs
@@ -15,14 +15,17 @@ namespace TybaStr.Core
     public struct FractionTask
     {
         [SerializeField] private Unit _unit;
-        public Unit Unit { get { return _unit; } }
+        public Unit Unit => _unit;
         [SerializeField] private TypeTask _type;
-        public TypeTask Type { get { return _type; } }
+        public TypeTask Type => _type;
+        [SerializeField] private Transform _target;
+        public Transform Target => _target;
 
-        public FractionTask(Unit unit,TypeTask type)
+        public FractionTask(Unit unit,TypeTask type,Transform target = null)
         {
             _unit = unit;
             _type = type;
+            _target = target;
         }
     }
 }
