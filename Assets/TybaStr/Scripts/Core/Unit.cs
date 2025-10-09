1346fe98ad83d6b4e0d2a4d26fe32ab3a56c0f6d
Wolex <wolexprag@gmail.com>
2025-10-09 16:37:05 +0300

    Added basic movement primitives: Move, asunc MoveTo, FindDirection

--- Assets/TybaStr/Scripts/Core/Unit.cs
+++ Assets/TybaStr/Scripts/Core/Unit.cs
@@ -8,7 +8,27 @@ namespace TybaStr.Core
     {
         [SerializeField] private UnitStats _stats;
         public event Action OnUnitStatsChanged;
+        [SerializeField] private Vector2 _to;
+        
+        public Vector2 FindDirection(Vector2 pos,Vector2 to)
+        {
+            return (to - pos).normalized;
+        }
+        public async Task MoveTo(Vector2 toLine)
+        {
+            _to = toLine;
+            Vector2 way = FindDirection(transform.position, _to);
+            while (true)
+            {
+                Move(way);
+                await Task.Yield();
+            }
 
+        }
+        public void Move(Vector2 input)
+        {
+            transform.Translate(input * _stats.Speed * Time.deltaTime);
+        }
 #if UNITY_EDITOR
         private void OnValidate()
 {
