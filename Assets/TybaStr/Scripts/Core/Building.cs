3f3d4f7c5251d6a66a24a334eeefd8d3985abd59
Wolex <wolexprag@gmail.com>
2025-10-22 21:33:50 +0300

    feat(Building): expose Belong as public property

--- Assets/TybaStr/Scripts/Core/Building.cs
+++ Assets/TybaStr/Scripts/Core/Building.cs
@@ -5,6 +5,6 @@ namespace TybaStr.Core
     public abstract class Building : MonoBehaviour
     {
         private Fraction _belong;
-        public Fraction Belong => _belong;
+        public Fraction Belong { get { return _belong; } set { _belong = value; } }
     }
 }
