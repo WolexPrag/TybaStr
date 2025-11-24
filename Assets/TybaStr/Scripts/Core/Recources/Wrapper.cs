6e332b9f998d749c278e11d2ab077bedf6d887f8
Wolex <wolexprag@gmail.com>
2025-11-24 13:26:19 +0300

    feat(Wrapper): add abstract class for wrappers

---
+++ Assets/TybaStr/Scripts/Core/Recources/Wrapper.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+namespace TybaStr.Core.Recources
+{
+    [System.Serializable]
+    public abstract class Wrapper : ScriptableObject
+    {
+        public abstract IResourceType Type { get; }
+        public abstract int Amount { get; }
+    }
+}
