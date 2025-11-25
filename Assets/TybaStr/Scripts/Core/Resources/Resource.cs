ff9cf8d2b229143859ad019e9946eac4c4ccec54
Wolex <wolexprag@gmail.com>
2025-11-25 12:09:25 +0300

    feat(Resource): implement Add/Take with CanAdd/CanTake checks

--- Assets/TybaStr/Scripts/Core/Resources/Resource.cs
+++ Assets/TybaStr/Scripts/Core/Resources/Resource.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using TybaStr.Core.Resources;
 
 namespace TybaStr.Core.Recources
@@ -12,5 +13,30 @@ namespace TybaStr.Core.Recources
         }
         public IResourceType Type { get; private set; }
         public int Amount { get; private set; }
+        public bool CanAdd(int value)
+        {
+            return value >= 0;
+        }
+        public bool CanTake(int value)
+        {
+            return value > 0 && value <= Amount;
+        }
+        public void Add(int value)
+        {
+            if (!CanAdd(value))
+            {
+                throw new Exception($"Can`t {nameof(Add)} value to {nameof(Resource)}.{nameof(Amount)}");
+            }
+            Amount += value;
+        }
+        public int Take(int value)
+        {
+            if (!CanTake(value))
+            {
+                throw new Exception($"Can`t {nameof(Take)} value from {nameof(Resource)}.{nameof(Amount)}");
+            }
+            Amount -= value;
+            return value;
+        }
     }
 }
