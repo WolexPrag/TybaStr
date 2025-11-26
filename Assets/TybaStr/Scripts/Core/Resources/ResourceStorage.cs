44f6c72edfa7f28c6d9a856348a8235c62507559
Wolex <wolexprag@gmail.com>
2025-11-26 16:58:07 +0300

    feat(ResourceStorage): add _resources field, SetResources and TryFindIndex

--- Assets/TybaStr/Scripts/Core/Resources/ResourceStorage.cs
+++ Assets/TybaStr/Scripts/Core/Resources/ResourceStorage.cs
@@ -1,4 +1,20 @@
-﻿public class ResourceStorage
+﻿using UnityEngine;
+using TybaStr.Core.Resources;
+using System.Collections.Generic;
+
+
+public class ResourceStorage
 {
-    
+    [SerializeField] private List<Resource> _resources = new();
+
+    public void SetResources(List<Resource> resources)
+    {
+        _resources = new List<Resource>(resources);
+    }
+
+    private bool TryFindIndex(IResourceType type, out int output)
+    {
+        output = _resources.FindIndex(f => f.Type == type);
+        return output != -1;
+    }
 }
