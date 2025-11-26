0b60fbc662ebc266f5ac50d3ce762bbefe40f0ef
Wolex <wolexprag@gmail.com>
2025-11-26 17:00:01 +0300

    feat(ResourceStorage): implement TryAdd/TryTake

--- Assets/TybaStr/Scripts/Core/Resources/ResourceStorage.cs
+++ Assets/TybaStr/Scripts/Core/Resources/ResourceStorage.cs
@@ -17,4 +17,32 @@ public class ResourceStorage
         output = _resources.FindIndex(f => f.Type == type);
         return output != -1;
     }
+    public bool TryAdd(Resource resource)
+    {
+        if (TryFindIndex(resource.Type, out int i))
+        {
+            _resources[i].Add(resource.Amount);
+        }
+        else
+        {
+            _resources.Add(resource);
+        }
+        return true;
+    }
+    public bool TryTake(in Resource request)
+    {
+        if (TryFindIndex(request.Type, out int i))
+        {
+            if (_resources[i].CanTake(request.Amount))
+            {
+                _resources[i].Take(request.Amount);
+                if (_resources[i].Amount == 0)
+                {
+                    _resources.RemoveAt(i);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
 }
